26aa0061d0212d104f222caaa37dfafb60a61c72
Sergio López <lopezs@ryanair.com>
2020-03-02 20:20:59 +0100

    Initial commit + entity definition

---
+++ src/test/java/com/shpl/flightbooking/FlightbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shpl.flightbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
